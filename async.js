cc9c38ad81d906ad6ae34dd3fb262b10456bee30
Aayush singh <aayushsingh81636@gmail.com>
2026-08-25 15:07:03 +0530

    demofetch.html

---
+++ async.js
@@ -0,0 +1,15 @@
+function register{
+
+    console.log("register here");
+}
+function login{
+    waitfordelay(3000);
+    console.log("login here");
+}
+function getData{
+    
+    console.log("Fetch data from DB");
+}
+function displayData {
+    console.log("display data");
+}
